26d85726b004610b953bdef2b65e205c2dcf88d7
Fazam16 <fadilzahran0123@gmail.com>
2022-09-06 23:16:15 +0700

    Ini adalah rancangan awal project saya

---
+++ latihWajah.py
@@ -0,0 +1,34 @@
+from asyncore import read
+import cv2, os, numpy as np
+from PIL import Image
+
+wajahDir = 'dataWajah' # Folder asal (src)
+latihDir = 'latihWajah' # Folder tujuan disimpannya training
+
+file = open('identitas/namaWong.txt', 'r')
+nama = file.read()
+file.close()
+
+def gambar(path): 
+    pathsGambar = [os.path.join(path, f) for f in os.listdir(path)]
+    samples = []
+    idMuka_ = []
+    for pathGambar in pathsGambar:
+        PILImg = Image.open(pathGambar).convert('L') # Convert ke dalam grey
+        imgNum = np.array(PILImg, 'uint8')
+        idMuka = int(os.path.split(pathGambar)[-1].split('.')[1])
+        faces = deteksiMuka.detectMultiScale(imgNum)
+        for (x, y, w, h) in faces:
+            samples.append(imgNum[y:y+h, x:x+w])
+            idMuka_.append(idMuka)
+            return samples, idMuka_
+
+faceRecognizer = cv2.face.LBPHFaceRecognizer_create() # algoritma LBPH
+deteksiMuka = cv2.CascadeClassifier('./fileXML/pendeteksiWajah.xml')
+
+print("Sedang melakukan training wajah")
+faces, IDs = gambar(wajahDir + '/' + nama)
+faceRecognizer.train(faces, np.array(IDs))
+
+faceRecognizer.write(latihDir + '/training.xml')
+print('Data wajah telah ditrainingkan')
---
+++ recordWajah.py
@@ -0,0 +1,60 @@
+from asyncore import read
+import cv2, os, speech_recognition as sr
+
+# Untuk memanggil kamera
+kamera = cv2.VideoCapture(1) 
+# Baca file dari gambar maka masukan src file gambar, jika dari webcam maka masukan index webcam yang kita miliki
+kamera.set(3, 640) # Untuk mengubah lebar kamera
+kamera.set(4, 480) # Untuk mengubah tinggi kamera
+
+deteksiWajah = cv2.CascadeClassifier('./fileXML/pendeteksiWajah.xml') # src xml mengenai deteksi wajah
+ambilData = 1
+wajahDir = 'dataWajah' #namaFolder
+
+mesinSuara = sr.Recognizer()
+mic = sr.Microphone() 
+hasil = ""
+
+with mic as source:
+    print("Sebut nama Anda")
+    rekaman = mesinSuara.listen(source) # Mesin melakukan rekaman suara dan memasukan hasil kedalam variabel rekaman
+    print("Ok, Terima kasih yaa")
+    try:
+        hasil = mesinSuara.recognize_google(rekaman, language="id-ID") # Memakai API google untuk melakukan rekaman suara, dan set bahasa indonesia
+        print(hasil)
+    except mesinSuara.UnkownValueError: # Jika suara tidak dapat dikenali oleh mesin maka mesin akan melakukan : 
+        print("Tidak dapat dideteksi")
+    except Exception as e:
+        print(e)
+
+namaWong = open('identitas/namaWong.txt', 'w')
+namaWong.write(hasil)
+namaWong.close
+
+buatFolder = r'./dataWajah/' + hasil 
+if not os.path.exists(buatFolder):
+    os.makedirs(buatFolder)
+
+while True: # Perulangan yang berguna untuk menangkap frame per secon
+    retV, frame = kamera.read() # Disini kamera membaca/merekam
+    warna = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Mengganti warna pada kamera
+    muka = deteksiWajah.detectMultiScale(warna,1.3, 5) # frame, scalefactor, min
+
+    for (x, y, w, h) in muka:
+        frame = cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 2) # Running retangle untuk mendeteksi wajah
+        namaFile = 'wajah ' + str(hasil) + '.' + str(ambilData) + '.jpg'
+        cv2.imwrite(wajahDir + '/' + hasil + '/' + namaFile, frame) # wajahDir = namaFolder yang dituju
+        ambilData+=1
+
+    cv2.imshow('webcam', frame) # Memanggil kamera untuk menampilkan output
+
+    filter = cv2.waitKey(1) & 0xFF 
+    if filter == 27 or filter == ord('q'): # Pada bagian ini untuk memberi keterangan button kamera untuk di stop
+        break
+    elif ambilData == 30:
+        break
+
+print("Program Selesai")
+kamera.release() # Release cache kamera ketika digunakan agar tidak memakan source pada komputer
+cv2.destroyAllWindows() # Menyelesaikan session
+#Closing pemanggil kamera
